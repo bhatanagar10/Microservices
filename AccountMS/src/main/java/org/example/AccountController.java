672a891f5b8ef61ce1ba661f83f8a64b60c56925
Aditya Bhatnagar <bhatanagar88.aditya@gmail.com>
2023-11-29 17:51:44 +0530

    created and execuetd account microservice

---
+++ AccountMS/src/main/java/org/example/AccountController.java
@@ -0,0 +1,20 @@
+package org.example;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/account")
+public class AccountController {
+
+    @Autowired
+    private Environment environment;
+
+    @GetMapping
+    public String getData(){
+        return environment.getProperty("local.server.port");
+    }
+}
